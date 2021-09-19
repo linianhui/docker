b981f2b4d5b8d25d648a315fc21c2ff529ed8ccc
lnh <lnhdyx@outlook.com>
2021-09-20 05:14:23 +0800

    update ci
    --ci openjdk/8u302

--- ci.sh
+++ ci.sh
@@ -1,32 +1,29 @@
-#!/usr/bin/zsh
-
 IMAGE_PATH_FILE=image.path.txt
 IMAGE_TAGS_FILE=image.tags.txt
+GREEN='\033[0;32m'
+END='\033[0m'
 
 function init(){
     rm $IMAGE_PATH_FILE
-    for tag in $@; do
-        echo $line
-        echo "${PWD}/image/${tag}" >> $IMAGE_PATH_FILE
+    for dir in $@; do
+        echo $dir >> $IMAGE_PATH_FILE
     done
 }
 
 function build(){
-    cat $IMAGE_PATH_FILE
     rm $IMAGE_TAGS_FILE
     while read line
     do
-        if [ -d "$line" ]; then
-            echo $line
-            version="$line:t"
-            dir=$(dirname "$line")
-            name="$dir:t"
-            tag="lnhcode/${name}:${version}"
-            ghcrTag="ghcr.io/linianhui/${name}:${version}"
-            url="org.opencontainers.image.url=https://github.com/linianhui/docker/tree/main/image/${name}/${version}"
-            docker build --tag $tag --tag $ghcrTag --label $url $line
-            echo $tag >> $IMAGE_TAGS_FILE
-            echo $ghcrTag >> $IMAGE_TAGS_FILE
+        dir="image/$line"
+        if [ -d "$dir" ]; then
+            tag="${line/\//:}"
+            tag1="lnhcode/$tag"
+            tag2="ghcr.io/linianhui/$tag"
+            label1="org.opencontainers.image.url=https://github.com/linianhui/docker/tree/main/$dir"
+            echo -e "\n\ndocker build --tag $GREEN$tag1$END --tag $GREEN$tag2$END --label $GREEN$label1$END $GREEN$dir$END \n"
+            docker build --tag $tag1 --tag $tag2 --label $label1 $dir
+            echo $tag1 >> $IMAGE_TAGS_FILE
+            echo $tag2 >> $IMAGE_TAGS_FILE
         fi
     done < $IMAGE_PATH_FILE
 }
@@ -35,23 +32,23 @@ function push(){
     cat $IMAGE_TAGS_FILE
     while read line
     do
-        echo $line
+        echo -e "\n\ndocker push $GREEN$line$END\n"
         docker push $line
     done < $IMAGE_TAGS_FILE
 }
 
 case $1 in
-    (init)
-    shift
-    init $*
-;;
-(build)
-shift
-build $*
-;;
-(push)
-shift
-push $*
-;;
+    init)
+        shift
+        init $*
+    ;;
+    build)
+        shift
+        build $*
+    ;;
+    push)
+        shift
+        push $*
+    ;;
 esac
 
