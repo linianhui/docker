a92b8ad2ef98f891cabf58354ce8c832a03b88ba
lnh <lnhdyx@outlook.com>
2022-01-30 12:43:16 +0800

    refactor-ci
    --ci python/3.8 openjdk/8u302

--- ci.sh
+++ ci.sh
@@ -1,14 +1,23 @@
-IMAGE_PATH=image
-REGISTRY=ghcr.io/linianhui
-LABEL_URL_PREFIX=org.opencontainers.image.url=https://github.com/linianhui/docker/tree/main
-LABEL_VERSION_PREFIX=org.opencontainers.image.version=https://github.com/linianhui/docker/commit
-LABEL_CREATED_NAME=org.opencontainers.image.created
+IMAGE_REGISTRY=ghcr.io/linianhui
+IMAGE_PATH_PREFIX=image
+IMAGE_AUTHER=lnhdyx@outlook.com
+IMAGE_REPO_URL_PREFIX=https://github.com/linianhui/docker/tree/main
+IMAGE_COMMIT_URl_PREFIX=https://github.com/linianhui/docker/commit
 
-IMAGE_PATH_FILE=image.path.txt
-IMAGE_TAGS_FILE=image.tags.txt
+IMAGE_PATH_FILE=image-path.txt
+IMAGE_TAGS_FILE=image-tags.txt
 
 GREEN='\033[0;32m'
 END='\033[0m'
+
+# https://github.com/opencontainers/image-spec/blob/main/annotations.md
+LABEL_AUTHERS=org.opencontainers.image.authors
+LABEL_URL=org.opencontainers.image.url
+LABEL_SOURCE=org.opencontainers.image.source
+LABEL_VERSION=org.opencontainers.image.version
+LABEL_CREATED=org.opencontainers.image.created
+LABEL_BASE_NAME=org.opencontainers.image.base.name
+LABEL_BASE_DIGEST=org.opencontainers.image.base.digest
 
 function init(){
     rm $IMAGE_PATH_FILE
@@ -18,19 +27,41 @@ function init(){
 }
 
 function build(){
+    COMMIT_SHA=$1
     rm $IMAGE_TAGS_FILE
     while read line
     do
-        dir="$IMAGE_PATH/$line"
+        dir="$IMAGE_PATH_PREFIX/$line"
         if [ -d "$dir" ]; then
             tag="${line/\//:}"
-            tag1="$REGISTRY/$tag"
-            labelUrl="$LABEL_URL_PREFIX/$dir"
-            labelVersion="$LABEL_VERSION_PREFIX/$1"
-            lableCreated="$LABEL_CREATED_NAME=$(date --iso-8601=seconds --utc)"
-            echo -e "\n\ndocker build --tag $GREEN$tag1$END --label $GREEN$labelUrl$END --label $GREEN$labelVersion$END --label $GREEN$lableCreated$END $GREEN$dir$END \n"
-            docker build --tag $tag1 --label $labelUrl --label $labelVersion --label $lableCreated $dir
-            echo $tag1 >> $IMAGE_TAGS_FILE
+            tagWithRegistry="$REGISTRY/$tag"
+            labelAuthers="$LABEL_AUTHERS=$IMAGE_AUTHER"
+            labelUrl="$LABEL_URL=$IMAGE_REPO_URL_PREFIX$dir"
+            labelSource="$LABEL_SOURCE=$IMAGE_REPO_URL_PREFIX$dir"
+            labelVersion="$LABEL_VERSION=$IMAGE_COMMIT_URl_PREFIX/$COMMIT_SHA"
+            lableCreated="$LABEL_CREATED=$(date --iso-8601=seconds --utc)"
+
+            echo -e '\n'
+            echo -e "\cdocker build \\"
+            echo -e "\c  --tag $GREEN$tagWithRegistry$END \\"
+            echo -e "\c  --label $GREEN$labelAuthers$END \\"
+            echo -e "\c  --label $GREEN$labelUrl$END \\"
+            echo -e "\c  --label $GREEN$labelSource$END \\"
+            echo -e "\c  --label $GREEN$labelVersion$END \\"
+            echo -e "\c  --label $GREEN$lableCreated$END \\"
+            echo -e "\c  $dir"
+            echo -e '\n'
+
+            docker build \
+            --tag $tagWithRegistry \
+            --label $labelAuthers \
+            --label $labelUrl \
+            --label $labelSource \
+            --label $labelVersion \
+            --label $lableCreated \
+            $dir
+
+            echo $tagWithRegistry >> $IMAGE_TAGS_FILE
         fi
     done < $IMAGE_PATH_FILE
 }
